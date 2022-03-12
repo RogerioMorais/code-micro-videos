f78f98bef729d961587151198cfa5e1e193fee60
Rogério Morais <rogeriofsantos@gmail.com>
2022-03-12 13:48:16 -0300

    Inicio da segunda perte do upload

--- tests/Traits/TestValidations.php
+++ tests/Traits/TestValidations.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\Traits;
 
 use Illuminate\Foundation\Testing\TestResponse;
+use Illuminate\Http\UploadedFile;
 
 trait TestValidations
 {
@@ -47,6 +48,34 @@ trait TestValidations
             $response->assertJsonFragment([
                 trans("validation.{$rule}", ['attribute' => $fieldName] + $ruleParams)
             ]);
+        }
+    }
+
+    protected function assertInvalidationFile($field,$extension,$maxSize,$rule,$ruleParams=[]){
+        $routes=[
+            [
+                "method"=>"POST",
+                'route'=>$this->routeStore()
+            ],
+            [
+                "method"=>"PUT",
+                'route'=>$this->routeUpdate()
+            ]
+        ];
+
+        foreach ($routes as $route) {
+            $file=UploadedFile::fake()->create("$field.1$extension");
+            $response=$this->json($route['method'],$route['route'],
+            [$field=>$file]);
+            
+            $this->assertInvalidationFields($response,[$field],$rule,$ruleParams);
+
+            $file=UploadedFile::fake()->create("$field.$extension")->size($maxSize+1);
+            $response=$this->json($route['method'],$route['route'],
+            [$field=>$file]);
+
+            $this->assertInvalidationFields($response,[$field],"max.file",['max'=>$maxSize]);
+            
         }
     }
 }
